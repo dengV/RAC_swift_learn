0d52cca84e7dc2bf0ce5a60041576fbaedc6b139
邓江洲 <dengjingzhou@zto.cn>
2018-03-23 16:43:43 +0800

    chap 2 ,challenge 好了

--- DoPractice.playground/Contents.swift
+++ DoPractice.playground/Contents.swift
@@ -6,3 +6,76 @@ import UIKit
 import RxSwift
 
 var str = "Hello, playground"
+
+
+public func example(of description: String, action: () -> Void) {
+    print("\n--- Example of:", description, "---")
+    action()
+}
+
+
+
+
+example(of: "Challenge ") {
+    let observable = Observable<Any>.never()
+    observable.do(onNext: { (ele) in
+        print("马上来")
+    }, onError: { (err) in
+        print("有错")
+    }, onCompleted: {
+        print("好啦")
+    }, onSubscribe: {
+        print("on  Subscribe")
+    }, onDispose: {
+        print("onDispose")
+    }).subscribe(onNext: { (element) in
+        print(element)
+    }, onCompleted: {
+        print("Completed")
+    })
+    
+    //do operator   -> RxSwift.Observable<Self.E>
+}
+
+
+
+
+example(of: "Challenge Two") {
+    let observable = Observable<Any>.never()
+    observable.debug("Challenge Two", trimOutput: true).subscribe(onNext: { (element) in
+        print(element)
+    }, onCompleted: {
+        print("Completed")
+    })
+}
+
+
+
+
+
+///
+
+
+example(of: "answer Challenge ") {
+    let observable = Observable<Any>.never()
+    observable.do(onSubscribe: {
+        print("on  Subscribe")
+    }).subscribe(onNext: { (element) in
+        print(element)
+    }, onCompleted: {
+        print("Completed")
+    }).addDisposableTo(DisposeBag())
+    
+    //do operator   -> RxSwift.Observable<Self.E>
+}
+
+
+
+example(of: "Answer Challenge Two") {
+    let observable = Observable<Any>.never()
+    observable.debug().subscribe(onNext: { (element) in
+        print(element)
+    }, onCompleted: {
+        print("Completed")
+    }).addDisposableTo(DisposeBag())
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # RAC_swift_learn
 RAC_swift_learn, 学习 Ray Wenderlich 的书RxSwift
+
+
+
+
+
+
+
+
+
+<hr>
+
+<hr>
+
+
+
+
+
+
+
+
+### git 操作
+
+
+> git push git@dengV:dengV/RAC_swift_learn.git
